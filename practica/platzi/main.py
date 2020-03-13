13ac7149dbd2de3dccfc9869b0e5c73542034ae7
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-12 18:05:55 -0600

    Final scraper and results

--- practica/platzi/main.py
+++ practica/platzi/main.py
@@ -1,6 +1,7 @@
 from common import get_config
-from models import CoursesListPage, CoursePage
+from models import CoursesListPage, CoursePage, ReviewsPages
 import re
+import pandas as pd
 
 course_page_link_re = re.compile(r'/clases/([\w-]*)/?$')
 
@@ -9,6 +10,13 @@ if __name__ == "__main__":
     for site_config in config['courses_sites'].values():
         for course_page_url in site_config['paths']:
             actual_courses_list_page = CoursesListPage(site_config['url'] + course_page_url, site_config)
+
+            reviews = []
             for course_link_raw in actual_courses_list_page.get_courses_list():
-                course_link = '/cursos/' + course_page_link_re.match(course_link_raw)[1]
+                course_link = course_link_raw  # '/cursos/' + course_page_link_re.match(course_link_raw)[1]
                 actual_course_page = CoursePage(course_link, site_config)
+
+                actual_course_review_page = ReviewsPages(actual_course_page.get_reviews_link(), site_config)
+                reviews.extend(actual_course_review_page.get_reviews())
+            df = pd.DataFrame(reviews)
+            df.to_csv('data.csv')
